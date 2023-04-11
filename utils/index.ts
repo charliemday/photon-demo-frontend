777ccee4db5f8e6af6a7499aacebbe58b3fdd8a7
charliemday <45036245+charliemday@users.noreply.github.com>
2023-04-11 10:04:58 +0000

    Pre-filled the sites based on the team

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 export { calculateSemrushCost } from "./calculateSemrushCost";
 export { capitalizeFirstLetter } from './capitalizeFirstLetter';
+export { cleanUrl } from './cleanUrl';
 export { resetQueryParams } from './resetQueryParams';
 export { typeCheckError } from './typeCheckError';
