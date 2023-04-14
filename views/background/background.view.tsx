e11cd73d79ab7fead85a0980fa458abffd32fb65
charliemday <45036245+charliemday@users.noreply.github.com>
2023-04-14 10:48:33 +0000

    Update background.view.tsx

--- views/background/background.view.tsx
+++ views/background/background.view.tsx
@@ -43,7 +43,7 @@ export const BackgroundView: React.FC<Props> = () => {
         bottom={0}
         bgColor="rgba(255, 255, 255, 0.75)"
       >
-        <MoonLoader size="sm" />
+        <MoonLoader size={40} />
       </Flex>
     </>
   );
