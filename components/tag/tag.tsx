c2314937a007777a61b4b2061d2f1df586b00b4e
James Edmonston <james@ethercreative.co.uk>
2023-05-17 21:14:57 +0100

    Add bgColor and textColor support to Tag

--- components/tag/tag.tsx
+++ components/tag/tag.tsx
@@ -3,10 +3,12 @@ import { FC } from "react";
 
 interface Props {
   text: string;
+  bgColor?: string;
+  textColor?: string;
 }
 
-export const Tag: FC<Props> = ({ text }) => (
-  <ChakraTag size="sm" height="20px">
+export const Tag: FC<Props> = ({ text, bgColor, textColor }) => (
+  <ChakraTag bgColor={bgColor} textColor={textColor} size="sm" height="20px">
     <TagLabel textAlign="center">{text}</TagLabel>
   </ChakraTag>
 );
