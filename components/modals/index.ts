ddf5771fa6350daa302516b432d44d6c4662873d
James Edmonston <james@ethercreative.co.uk>
2023-05-18 07:33:12 +0100

    Add SimpleModal component

--- components/modals/index.ts
+++ components/modals/index.ts
@@ -1,5 +1,6 @@
-export { AddTeamModal } from './add-team.modal';
-export { ConfirmationModal } from './confirmation.modal';
-export { FeedbackModal } from './feedback.modal';
+export { AddTeamModal } from "./add-team.modal";
+export { ConfirmationModal } from "./confirmation.modal";
+export { FeedbackModal } from "./feedback.modal";
 export { Modal } from "./modal";
-export { OAuthConnectingModal } from './oauth-connecting.modal';
+export { OAuthConnectingModal } from "./oauth-connecting.modal";
+export { SimpleModal } from "./simple.modal";
