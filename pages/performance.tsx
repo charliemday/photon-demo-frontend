451f6548ab015176df0a3d0d5e86515ecd99ea43
charliemday <45036245+charliemday@users.noreply.github.com>
2023-05-19 15:30:02 +0000

    Fixed 404 on Performance page

---
+++ pages/performance.tsx
@@ -0,0 +1,15 @@
+import { SidebarLayout } from "components/layouts";
+import { FC } from "react";
+
+const Performance: FC = () => {
+  return (
+    <SidebarLayout
+      headerTitle="Baser | Performance"
+      title="(Performance Page) Under Construction 🏗️"
+    >
+       
+    </SidebarLayout>
+  );
+};
+
+export default Performance;
