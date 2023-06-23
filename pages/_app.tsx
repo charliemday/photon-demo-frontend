7c34b3f7c2d9f2dca3b5d855425c36c34fe6eb49
charliemday <45036245+charliemday@users.noreply.github.com>
2023-06-23 12:10:10 +0000

    Added Hotjar Scripts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
+import { IS_PROD } from "config";
 import Script from "next/script";
 import { persistor, store } from "store";
 import { theme } from "styles";
@@ -22,12 +23,23 @@ function MyApp({ Component, pageProps }: AppProps) {
         defer
       />
       {/* Rewardful */}
-      <Script
-        id="rewardful"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`,
-        }}
-      />
+      {IS_PROD && (
+        <Script
+          id="rewardful"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,r){w._rwq=r;w[r]=w[r]||function(){(w[r].q=w[r].q||[]).push(arguments)}})(window,'rewardful');`,
+          }}
+        />
+      )}
+      {/* Hotjar */}
+      {IS_PROD && (
+        <Script
+          id="hotjar"
+          dangerouslySetInnerHTML={{
+            __html: `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:3546670,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
+          }}
+        />
+      )}
       <Script src="https://r.wdfl.co/rw.js" data-rewardful="449653" />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
