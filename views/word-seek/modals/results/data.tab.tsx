1a9732aa3270ccd0b0a8b35b33dfaeb8b6dd9597
charliemday <45036245+charliemday@users.noreply.github.com>
2023-07-20 11:43:06 +0000

    Update data.tab.tsx

--- views/word-seek/modals/results/data.tab.tsx
+++ views/word-seek/modals/results/data.tab.tsx
@@ -59,7 +59,7 @@ export const DataTab: FC<Props> = ({ data, exportData, jobGroup }) => {
       return data.missingKeywords
         .map((i) => i)
         .filter((i) => i.position < POSITION_LIMIT)
-        .sort((a, b) => a.position - b.position)
+        .sort((a, b) => b.impressions - a.impressions)
         .slice(0, KEYWORD_LIMIT);
     }
 
